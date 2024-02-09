1f0ed4cac49d94c2098d24b328099aa9d54d32bb
Fromjoga <89554266+Fromjoga@users.noreply.github.com>
2024-02-09 09:30:54 -0300

    Update script.js

--- apps/script.js
+++ apps/script.js
@@ -1,5 +1,2 @@
-function down(id,link) {if(document.documentElement.clientHeight >= 700){window.open(link, "_blank");return;}const dd = document.getElementById(id);window.scroll({top: dd.getBoundingClientRect().top + window.pageYOffset,behavior: 'smooth'});}
-setInterval(function() {
-fetch('https://249567ab-56cd-4f3c-a399-963648b0ebcf-00-2dw2nucx8yjbl.kirk.replit.dev/index.html')
-}, 5 * 60 * 1000);
+function down(id,link) {if(document.documentElement.clientHeight >= 650 && document.documentElement.clientHeight =< 1024){window.open(link, "_blank");return;}const dd = document.getElementById(id);window.scroll({top: dd.getBoundingClientRect().top + window.pageYOffset,behavior: 'smooth'});}
 function link(l) {window.open(l, "_blank")}
