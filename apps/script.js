9af6a194acd3168beaaea07014c0db8ab30d8baa
Fromjoga <89554266+Fromjoga@users.noreply.github.com>
2024-04-25 15:41:16 -0300

    Update script.js

--- apps/script.js
+++ apps/script.js
@@ -1,5 +1,2 @@
 function down(id,link) {if(document.documentElement.clientHeight < 900 && document.documentElement.clientHeight > 700){window.open(link, "_blank");return;}const dd = document.getElementById(id);window.scroll({top: dd.getBoundingClientRect().top + window.pageYOffset,behavior: 'smooth'});}
 function link(l) {window.open(l, "_blank")}
-        window.onload = function() {
-                        window.location.href = '404.html';
-        };
