564f3fa3ef33f823bf17678dab1e8823eefa1b14
Fromjoga <89554266+Fromjoga@users.noreply.github.com>
2024-06-02 09:17:20 -0300

    Atualizar o script.js

--- apps/script.js
+++ apps/script.js
@@ -1,16 +1,14 @@
 function down(id,link) {if(document.documentElement.clientHeight < 900 && document.documentElement.clientHeight > 700){window.open(link, "_blank");return;}const dd = document.getElementById(id);window.scroll({top: dd.getBoundingClientRect().top + window.pageYOffset,behavior: 'smooth'});}
 function link(l) {window.open(l, "_blank")}
-
         function isElementInViewport(el) {
             var rect = el.getBoundingClientRect();
             return (
                 rect.top >= 0 &&
-                rect.left >= 0 &&
+                rect.left >= 0 ||
                 rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
                 rect.right <= (window.innerWidth || document.documentElement.clientWidth)
             );
         }
-
         function showVisibleElements() {
             var elements = document.querySelectorAll('div');
             elements.forEach(function(element) {
@@ -20,6 +18,5 @@ function link(l) {window.open(l, "_blank")}
                 }
             });
         }
-
         window.addEventListener('load', showVisibleElements);
         window.addEventListener('scroll', showVisibleElements);
