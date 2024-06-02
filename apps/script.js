863b695f8684b43c207199a34af581fe0b886238
Fromjoga <89554266+Fromjoga@users.noreply.github.com>
2024-06-02 09:19:52 -0300

    Atualizar o script.js

--- apps/script.js
+++ apps/script.js
@@ -3,8 +3,6 @@ function link(l) {window.open(l, "_blank")}
         function isElementInViewport(el) {
             var rect = el.getBoundingClientRect();
             return (
-                rect.top >= 0 ||
-                rect.left >= 0 ||
                 rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
                 rect.right <= (window.innerWidth || document.documentElement.clientWidth)
             );
