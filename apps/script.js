f7710abfa202b16851c0527bf1350367de526edf
Fromjoga <89554266+Fromjoga@users.noreply.github.com>
2024-06-03 13:51:55 -0300

    Atualizar o script.js

--- apps/script.js
+++ apps/script.js
@@ -2,11 +2,10 @@ function down(id,link) {if(document.documentElement.clientHeight < 900 && docume
 function link(l) {window.open(l, "_blank")}
 document.addEventListener("DOMContentLoaded", function() {
             const paginaPrincipal = 'index.html';
-            const urlProibidas = ['script.js', 'style.css'];
 
             const caminhoAtual = window.location.pathname.split('/').pop();
 
-            if ((urlProibidas.includes(caminhoAtual)) || (caminhoAtual !== '' && caminhoAtual !== paginaPrincipal && caminhoAtual !== '404.html')) {
+            if (caminhoAtual !== '' && caminhoAtual !== paginaPrincipal && caminhoAtual !== '404.html') {
                 window.location.href = '404.html';
             }
         });
