e3e601ef9556500e57f2bd881c6aa88898d50f8e
Fromjoga <89554266+Fromjoga@users.noreply.github.com>
2024-06-08 21:12:16 -0300

    Atualizar o scripttt.js

--- apps/scripttt.js
+++ apps/scripttt.js
@@ -9,3 +9,21 @@ document.addEventListener("DOMContentLoaded", function() {
                 window.location.href = '404.html';
             }
         });
+document.onkeydown = function(e) {
+    if (e.keyCode === 123 || // F12
+        (e.ctrlKey && e.shiftKey && e.keyCode === 73) || // Ctrl+Shift+I
+        (e.ctrlKey && e.shiftKey && e.keyCode === 74) || // Ctrl+Shift+J
+        (e.ctrlKey && e.keyCode === 85)) { // Ctrl+U
+      return false;
+    }
+  };
+
+  (function() {
+    var element = new Image();
+    Object.defineProperty(element, 'id', {
+      get: function() {
+        alert('Console detectado!');
+      }
+    });
+    console.log(element);
+  })();
